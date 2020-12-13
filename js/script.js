8c3382a1a40b99c9af363d74618e3115fedd654b
Alexey Sreda <alexey.sreda@gmail.com>
2020-12-13 20:55:03 +0700

    questions page added

--- js/script.js
+++ js/script.js
@@ -1,56 +1,16 @@
-// "use strict";
-//
-// var emblaNode = document.getElementById("embla");
-// var embla = EmblaCarousel(emblaNode, {
-//     // align: "start",
-//     loop: "true"
-// });
-//
-//
-// var prevEmbla = document.getElementById('emblaButtonPrev');
-// prevEmbla.addEventListener('click', embla.scrollPrev);
-// var nextEmbla = document.getElementById('emblaButtonNext');
-// nextEmbla.addEventListener('click', embla.scrollNext);
-//
-// var autoplay = function autoplay(embla, interval) {
-//     var timer = 0;
-//
-//     var play = function play() {
-//         stop();
-//         requestAnimationFrame(function () {
-//             return timer = window.setTimeout(next, interval);
-//         });
-//     };
-//
-//     var stop = function stop() {
-//         window.clearTimeout(timer);
-//         timer = 0;
-//     };
-//
-//     var next = function next() {
-//         if (embla.canScrollNext()) {
-//             embla.scrollNext();
-//         } else {
-//             embla.scrollTo(0);
-//         }
-//
-//         play();
-//     };
-//
-//     return {
-//         play: play,
-//         stop: stop
-//     };
-// };
-//
-// var autoplayer = autoplay(embla, 3000);
-// embla.on("pointerDown", autoplayer.stop);
-// embla.on("init", autoplayer.play);
-//
-//
-// // var emblaButtons = Array.from(document.getElementsByClassName('embla__button'));
-// // emblaButtons.forEach(function (el) {
-// //     el.addEventListener('click', function () {
-// //         el.blur();
-// //     });
-// // });
+"use strict";
+
+
+
+
+//questions page
+
+let questions = Array.from(document.getElementsByClassName('questions__item'));
+questions.forEach(function(el) {
+    let questionLink = el.getElementsByClassName('questions__title')[0];
+    questionLink.addEventListener('click', function(question) {
+        question.preventDefault();
+        el.classList.toggle('questions__item_active');
+    });
+});
+
