c3d31830dbff5285a4bfc2a48607ce325afe083d
Alexey Sreda <alexey.sreda@gmail.com>
2021-01-13 17:07:15 +0700

    calculator moved to calc.js

---
+++ js/calc.js
@@ -0,0 +1,69 @@
+//calculator
+'use strict';
+
+
+let minPrice = 0;
+let time = 0;
+
+let weight = 0;
+let volume = 0;
+
+let length = 0;
+let width = 0;
+let height = 0;
+
+let factVolume = volume * 1.1;
+
+
+if (weight/factVolume < 215) {
+    // считаем по объёму
+}
+
+else {
+    // считаем по весу
+}
+
+
+
+
+
+
+const autoMY = [
+    {
+        minPrice: 1500,
+        time: '12 - 13 дней'
+    },
+    {
+        
+    },
+    {
+
+    }
+];
+
+const autoNY = [
+    {
+        minPrice: 1000,
+        time: '8 - 9 дней'
+    }
+];
+
+const containerMY = [
+    {
+        minPrice: 700,
+        time: 'по запросу'
+    }
+
+];
+
+const containerNY = [
+    {
+        minPrice: 500,
+        time: 'по запросу'
+    }
+
+];
+
+
+
+
