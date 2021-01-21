8cb931f51082141616e6566944d072158d4bae5d
alexeypensar <alexey@pensar.ru>
2021-01-21 11:07:12 +0700

    gulp added, production

---
+++ src/js/calc.js
@@ -0,0 +1,327 @@
+//calculator
+'use strict';
+
+// прайс - авто из Москвы в Якутск
+const autoMY = {
+    volumeToWeight: 215,
+    minPrice: 1500,
+    time: '12 - 13 дней',
+    volumePrice: [8500, 8200, 8000, 7800, 7600, 7500],
+    weightPrice: [40, 39.5, 39, 38.5, 38, 37]
+};
+
+// прайс - авто из Новосибирска в Якутск
+const autoNY = {
+    volumeToWeight: 215,
+    minPrice: 1000,
+    time: '8 - 9 дней',
+    volumePrice: [6000, 5500, 5300, 5100, 5000, 4900],
+    weightPrice: [25, 24, 23.75, 23.5, 23.25, 23]
+};
+
+
+// прайс - контейнер из Москвы в Якутск
+const containerMY = {
+    volumeToWeight: 280,
+    minPrice: 700,
+    time: 'по запросу',
+    volumePrice: [6200, 6000, 5600, 5400, 6000, 5600, 5400, 5300, 5600, 5400, 5300, 5250, 5400, 5300, 5250, 5200],
+    weightPrice: [16, 17, 18, 19, 15.8, 16.5, 17, 18, 15.5, 16, 16.5, 17, 15, 15.5, 15.8, 16]
+};
+
+// прайс - контейнер из Новосибирска в Якутск
+const containerNY = {
+    volumeToWeight: 280,
+    minPrice: 500,
+    time: 'по запросу',
+    volumePrice: [4800, 4700, 4600, 4500, 4700, 4600, 4500, 4450, 4600, 4500, 4450, 4425, 4500, 4450, 4425, 4400],
+    weightPrice: [15, 16, 17, 18, 14.8, 15.15, 16, 17, 14.5, 14.8, 15.5, 16, 14, 14.5, 14.8, 15]
+};
+
+
+
+
+
+
+
+// weight.addEventListener('change', function() {
+//     console.log(weight.value);
+// })
+//
+// console.log(weight);
+
+
+let calcFrom = document.getElementById('calcFrom');
+let calcTo = document.getElementById('calcTo');
+
+let calcButton = document.getElementById('calcButton');
+let calcResults = document.getElementById('calcResults');
+let calcAutoPrice = document.getElementById('calcAutoPrice');
+let calcAutoTime = document.getElementById('calcAutoTime');
+let calcContainerPrice = document.getElementById('calcContainerPrice');
+
+let calcWeight = document.getElementById('calcWeight');
+let calcVolume = document.getElementById('calcVolume');
+
+
+// калькулятор для автоперевозок:
+
+let calcAuto = function (type, weight, volume) {
+    weight = +calcWeight.value;
+    volume = +calcVolume.value;
+
+
+
+    let factVolume = (volume * 1.1).toFixed(2);
+
+
+    let basePrice;
+
+    let resultPrice = 0;
+
+
+    // по объёму:
+    if (weight / factVolume < type.volumeToWeight) {
+
+        basePrice = type.volumePrice;
+
+
+        if (factVolume < 1) {
+            resultPrice = factVolume * basePrice[0];
+        } else if (factVolume >= 1 && factVolume < 3) {
+            resultPrice = factVolume * basePrice[1];
+        } else if (factVolume >= 3 && factVolume < 5) {
+            resultPrice = factVolume * basePrice[2];
+        } else if (factVolume >= 5 && factVolume < 10) {
+            resultPrice = factVolume * basePrice[3];
+        } else if (factVolume >= 10 && factVolume < 15) {
+            resultPrice = factVolume * basePrice[4];
+        } else if (factVolume >= 15) {
+            resultPrice = factVolume * basePrice[5];
+        }
+    }
+
+    // по весу:
+    else {
+
+        basePrice = type.weightPrice;
+
+        if (weight < 200) {
+            resultPrice = weight * basePrice[0];
+        } else if (weight >= 200 && weight < 600) {
+            resultPrice = weight * basePrice[1];
+        } else if (weight >= 600 && weight < 1000) {
+            resultPrice = weight * basePrice[2];
+        } else if (weight >= 1000 && weight < 2000) {
+            resultPrice = weight * basePrice[3];
+        } else if (weight >= 2000 && weight < 3000) {
+            resultPrice = weight * basePrice[4];
+        } else if (weight >= 3000) {
+            resultPrice = weight * basePrice[5];
+        }
+    }
+
+    if (resultPrice < type.minPrice) {
+        resultPrice = type.minPrice;
+    }
+
+    calcAutoPrice.innerText = resultPrice.toFixed().toString();
+    calcAutoTime.innerText = type.time;
+
+
+
+
+};
+
+// калькулятор для контейнерных перевозок:
+let calcContainer = function (type, weight, volume) {
+
+    weight = +calcWeight.value;
+    volume = +calcVolume.value;
+
+
+
+    let factVolume = (volume * 1.1).toFixed(2);
+
+
+    let basePrice;
+
+    let resultPrice = 0;
+
+    let factVolumeToWeight = weight / factVolume;
+
+
+
+
+    // по объёму:
+    if (factVolumeToWeight < type.volumeToWeight) {
+
+        basePrice = type.volumePrice;
+
+
+        if (factVolume <= 2 && factVolumeToWeight > 240) {
+            resultPrice = factVolume * basePrice[0];
+        }
+        else if (factVolume <= 2 && factVolumeToWeight > 200 && factVolumeToWeight <= 240) {
+            resultPrice = factVolume * basePrice[1];
+        }
+        else if (factVolume <= 2 && factVolumeToWeight > 160 && factVolumeToWeight <= 200) {
+            resultPrice = factVolume * basePrice[2];
+        }
+        else if (factVolume <= 2 && factVolumeToWeight <= 160) {
+            resultPrice = factVolume * basePrice[3];
+        }
+
+        else if (factVolume > 2 && factVolume <= 4 && factVolumeToWeight > 240) {
+            resultPrice = factVolume * basePrice[4];
+        }
+        else if (factVolume > 2 && factVolume <= 4 && factVolumeToWeight > 200 && factVolumeToWeight <= 240) {
+            resultPrice = factVolume * basePrice[5];
+        }
+        else if (factVolume > 2 && factVolume <= 4 && factVolumeToWeight > 160 && factVolumeToWeight <= 200) {
+            resultPrice = factVolume * basePrice[6];
+        }
+        else if (factVolume > 2 && factVolume <= 4 && factVolumeToWeight <= 160) {
+            resultPrice = factVolume * basePrice[7];
+        }
+
+        else if (factVolume > 4 && factVolume <= 12 && factVolumeToWeight > 240) {
+            resultPrice = factVolume * basePrice[8];
+        }
+        else if (factVolume > 4 && factVolume <= 12 && factVolumeToWeight > 200 && factVolumeToWeight <= 240) {
+            resultPrice = factVolume * basePrice[9];
+        }
+        else if (factVolume > 4 && factVolume <= 12 && factVolumeToWeight > 160 && factVolumeToWeight <= 200) {
+            resultPrice = factVolume * basePrice[10];
+        }
+        else if (factVolume > 4 && factVolume <= 12 && factVolumeToWeight <= 160) {
+            resultPrice = factVolume * basePrice[11];
+        }
+
+        else if (factVolume > 12  && factVolumeToWeight > 240) {
+            resultPrice = factVolume * basePrice[12];
+        }
+        else if (factVolume > 12  && factVolumeToWeight > 200 && factVolumeToWeight <= 240) {
+            resultPrice = factVolume * basePrice[13];
+        }
+        else if (factVolume > 12  && factVolumeToWeight > 160 && factVolumeToWeight <= 200) {
+            resultPrice = factVolume * basePrice[14];
+        }
+        else if (factVolume > 12  && factVolumeToWeight <= 160) {
+            resultPrice = factVolume * basePrice[15];
+        }
+
+    }
+
+    // по весу:
+    else {
+
+        basePrice = type.weightPrice;
+
+
+        if (weight <= 500 && factVolumeToWeight > 400) {
+            resultPrice = weight * basePrice[0];
+        }
+        else if (weight <= 500 && factVolumeToWeight > 360 && factVolumeToWeight <= 400) {
+            resultPrice = weight * basePrice[1];
+        }
+        else if (weight <= 500 && factVolumeToWeight > 320 && factVolumeToWeight <= 360) {
+            resultPrice = weight * basePrice[2];
+        }
+        else if (weight <= 500 && factVolumeToWeight <= 320) {
+            resultPrice = weight * basePrice[3];
+        }
+
+        else if (weight > 500 && weight <= 1000 && factVolumeToWeight > 400) {
+            resultPrice = weight * basePrice[4];
+        }
+        else if (weight > 500 && weight <= 1000 && factVolumeToWeight > 360 && factVolumeToWeight <= 400) {
+            resultPrice = weight * basePrice[5];
+        }
+        else if (weight > 500 && weight <= 1000 && factVolumeToWeight > 320 && factVolumeToWeight <= 360) {
+            resultPrice = weight * basePrice[6];
+        }
+        else if (weight > 500 && weight <= 1000 && factVolumeToWeight <= 320) {
+            resultPrice = weight * basePrice[7];
+        }
+
+        else if (weight > 1000 && weight <= 3000 && factVolumeToWeight > 400) {
+            resultPrice = weight * basePrice[8];
+        }
+        else if (weight > 1000 && weight <= 3000 && factVolumeToWeight > 360 && factVolumeToWeight <= 400) {
+            resultPrice = weight * basePrice[9];
+        }
+        else if (weight > 1000 && weight <= 3000 && factVolumeToWeight > 320 && factVolumeToWeight <= 360) {
+            resultPrice = weight * basePrice[10];
+        }
+        else if (weight > 1000 && weight <= 3000 && factVolumeToWeight <= 320) {
+            resultPrice = weight * basePrice[11];
+        }
+
+        else if (weight > 3000  && factVolumeToWeight > 400) {
+            resultPrice = weight * basePrice[12];
+        }
+        else if (weight > 3000  && factVolumeToWeight > 360 && factVolumeToWeight <= 400) {
+            resultPrice = weight * basePrice[13];
+        }
+        else if (weight > 3000  && factVolumeToWeight > 320 && factVolumeToWeight <= 360) {
+            resultPrice = weight * basePrice[14];
+        }
+        else if (weight > 3000  && factVolumeToWeight <= 320) {
+            resultPrice = weight * basePrice[15];
+        }
+    }
+
+    if (resultPrice < type.minPrice) {
+        resultPrice = type.minPrice;
+
+    }
+
+
+    calcContainerPrice.innerText = resultPrice.toFixed().toString();
+
+};
+
+
+
+
+
+
+calcButton.addEventListener('click', function() {
+
+    let calcTest = [calcFrom, calcTo, calcWeight, calcVolume];
+    let calcTestErrors = 0;
+
+    for (let i = 0; i < calcTest.length; i++) {
+
+        if (calcTest[i].value === '') {
+            // calcTest[i].style.border = '1px solid red';
+            calcTestErrors += 1;
+        }
+    }
+
+    if (calcTestErrors === 0) {
+
+
+        if (calcFrom.value === 'Novosibirsk') {
+            calcAuto(autoNY);
+            calcContainer(containerNY);
+        }
+
+        else if (calcFrom.value === 'Moscow') {
+            calcAuto(autoMY);
+            calcContainer(containerMY);
+        }
+
+        calcResults.style.display = 'block';
+        calcResults.scrollIntoView();
+    }
+
+    else {
+        alert('Заполните все поля');
+    }
+
+
+
+
+});
---
+++ src/js/script.js
@@ -0,0 +1,126 @@
+"use strict";
+
+
+//mobile menu
+
+const mobileMenuButton = document.getElementById('mobileMenuButton');
+const mobileMenuCloseButton = document.getElementById('mobileMenuCloseButton');
+const mobileMenu = document.getElementById('mobileMenu');
+
+const shadow = document.getElementById('shadow');
+
+mobileMenuButton.addEventListener('click', function() {
+    mobileMenu.classList.toggle('header__ul_active');
+    shadow.style.display = 'flex';
+});
+
+mobileMenuCloseButton.addEventListener('click', function() {
+    mobileMenu.classList.toggle('header__ul_active');
+    shadow.style.display = 'none';
+});
+
+
+//closing mobile menu on click to link
+let mobileMenuLinks = Array.from(document.getElementsByClassName('header__link'));
+
+mobileMenuLinks.forEach(function(el) {
+    el.addEventListener('click', function() {
+        mobileMenu.classList.remove('header__ul_active');
+        shadow.style.display = 'none';
+    })
+});
+
+
+shadow.addEventListener('click', function() {
+    mobileMenu.classList.remove('header__ul_active');
+    shadow.style.display = 'none';
+})
+
+
+
+//questions page
+
+let questions = Array.from(document.getElementsByClassName('questions__item'));
+questions.forEach(function(el) {
+    let questionLink = el.getElementsByClassName('questions__title')[0];
+    questionLink.addEventListener('click', function(question) {
+        question.preventDefault();
+        el.classList.toggle('questions__item_active');
+    });
+});
+
+
+
+// select in footer, changing phone numbers, address and so on
+
+let footerSelect = document.getElementById('footerCity');
+
+let cityAddress = document.getElementById('cityAddress');
+let cityPhone = document.getElementById('cityPhone');
+let cityPhone2 = document.getElementById('cityPhone2');
+// let cityPhone3 = document.getElementById('cityPhone3');
+// let cityPhone4 = document.getElementById('cityPhone4');
+
+
+
+
+footerSelect.addEventListener('change', function() {
+    if (footerSelect.value === 'Novosibirsk') {
+        cityAddress.textContent = 'ул. Новая Заря 2А корп.6';
+
+        // cityPhone.textContent = '8 (383) 280-41-71';
+        cityPhone.textContent = '8-913-382-67-40';
+        cityPhone2.textContent = '8-983-136-85-58';
+        // cityPhone4.textContent = '8-983-003-87-79';
+
+        cityPhone.setAttribute('href', 'tel:+79133826740');
+        cityPhone2.setAttribute('href', 'tel:+79831368558');
+        // cityPhone3.setAttribute('href', 'tel:+79831368558');
+        // cityPhone4.setAttribute('href', 'tel:+79830038779');
+
+        cityPhone2.style.visibility = 'visible';
+        // cityPhone3.style.visibility = 'visible';
+        // cityPhone4.style.visibility = 'visible';
+
+        cityPhone2.style.pointerEvents = 'auto';
+        // cityPhone3.style.pointerEvents = 'auto';
+        // cityPhone4.style.pointerEvents = 'auto';
+
+
+
+    }
+    else if (footerSelect.value === 'Moscow') {
+        cityAddress.textContent = 'р-н Некрасовка, ул.Вольская 1я, владение 23 стр.4';
+        cityPhone.textContent = '8-985-804-72-82';
+        cityPhone.setAttribute('href', 'tel:+79858047282');
+
+        cityPhone2.style.visibility = 'hidden';
+        cityPhone2.style.pointerEvents = 'none';
+        // cityPhone3.style.visibility = 'hidden';
+        // cityPhone3.style.pointerEvents = 'none';
+        // cityPhone4.style.visibility = 'hidden';
+        // cityPhone4.style.pointerEvents = 'none';
+
+
+    }
+    else if (footerSelect.value === 'Yakutsk') {
+        cityAddress.textContent = 'г. Якутск, ул. Пилотов, 22';
+
+        cityPhone.textContent = '8 (4112) 23-14-04';
+        cityPhone.setAttribute('href', 'tel:+74112231404');
+        cityPhone2.style.visibility = 'visible';
+        cityPhone2.style.pointerEvents = 'auto';
+        cityPhone2.textContent = '8-914-300-38-19';
+        cityPhone2.setAttribute('href', 'tel:+79143003819');
+
+
+        // cityPhone3.style.visibility = 'hidden';
+        // cityPhone3.style.pointerEvents = 'none';
+        // cityPhone4.style.visibility = 'hidden';
+        // cityPhone4.style.pointerEvents = 'none';
+    }
+});
+
+
+// todo во всех футерах и хэдерах на всех страницах всё проверить, дёрнуть с index.html
+
